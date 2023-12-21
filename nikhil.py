72ce56ebad4bb70890e4432c0a9202f8654778ba
nikhilsaps <nikhilsuccess6@gmail.com>
2023-12-21 17:03:43 +0530

    day 13_1

--- nikhil.py
+++ nikhil.py
@@ -1,2 +1,13 @@
 #the code will implement the class
 
+class car:
+    def  __init__(self,name ,year):
+        self.name =name
+        self.year=year
+
+
+mycar=car("ford",1993)
+yourcar=car("nissan",2023)
+
+
+print(yourcar.year)
