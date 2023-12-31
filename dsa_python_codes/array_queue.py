3efcba1c3a739b7d5e2d17175f866df0ddffe900
nikhilsaps <nikhilsuccess6@gmail.com>
2023-12-31 10:12:08 +0530

    array to DSA

--- dsa_python_codes/array_queue.py
+++ dsa_python_codes/array_queue.py
@@ -1,1 +1,33 @@
-print("hello")
+# the program will implement all the function of queue in  array
+
+
+class queue:
+    def __init__(self,lst=[]):
+        self.lst=lst
+    def __str__(self):
+        return f"{self.lst}"
+
+    def add(self,num):
+        self.lst.append(num)
+
+    def show(self):
+        for x in self.lst:
+            print(f"{x} \n")
+    def remove(self,indi):
+         self.lst.pop(indi)
+    def update(self,indi,num):
+        self.lst[indi]=num
+
+    def clear(self, clear):
+        self.lst.clear()
+
+
+
+def main():
+    p=queue()
+    p.add
+
+
+
+if __name__ =="__main__":
+    main()
